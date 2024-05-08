957ab1ab90dd8555df9b24dc6b6d180b86252237
vugarismayil24 <mrsoldier799@gmail.com>
2024-05-08 15:54:04 +0400

    Add: Bottom Navigation Bar

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: prefer_const_constructors
 
 import 'package:cofee_delivery_app/model/coffee_model.dart';
 import 'package:cofee_delivery_app/model/coffee_size_enum.dart';
 import 'package:cofee_delivery_app/screens/detail/widgets/detail_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:readmore/readmore.dart';
 
 class DetailScreen extends StatelessWidget {
@@ -14,155 +16,218 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffFFFFFF),
       appBar: const DetailAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.network(
+                    product.imageUrl.toString(),
+                    width: double.infinity,
+                    height: 250,
+                    fit: BoxFit.fitWidth,
+                    filterQuality: FilterQuality.low,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        product.title.toString(),
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        product.subTitle.toString(),
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xff9B9B9B),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "⭐${product.rating}",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            " (${product.voteCount})",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: Color(0xffF9F9F9),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: Icon(
+                              Icons.coffee,
+                              color: Color(0xffC67C4E),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                color: Color(0xffF9F9F9),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: Icon(
+                              Icons.coffee,
+                              color: Color(0xffC67C4E),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: Colors.black,
+                        thickness: .5,
+                      ),
+                      Text(
+                        "Description",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      ReadMoreText(
+                        "${product.description}",
+                        trimMode: TrimMode.Line,
+                        trimLines: 1,
+                        colorClickableText: Color(0xffC67C4E),
+                        trimCollapsedText: 'Read more',
+                        trimExpandedText: 'Show less',
+                        moreStyle: TextStyle(
+                          color: Color(0xffC67C4E),
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Size",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      Row(
+                        children: [
+                          CoffeeSizesButtonwidget(
+                            size: CoffeeSizes.S
+                                .toString()
+                                .split("CoffeeSizes.")
+                                .join(),
+                          ),
+                          CoffeeSizesButtonwidget(
+                            size: CoffeeSizes.M
+                                .toString()
+                                .split("CoffeeSizes.")
+                                .join(),
+                          ),
+                          CoffeeSizesButtonwidget(
+                            size: CoffeeSizes.L
+                                .toString()
+                                .split("CoffeeSizes.")
+                                .join(),
+                          ),
+                        ],
+                      ),
+                      // SizedBox(
+                      //   height: 96,
+                      // ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                product.imageUrl.toString(),
-                width: double.infinity,
-                height: 250,
-                fit: BoxFit.fitWidth,
-                filterQuality: FilterQuality.low,
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Column(
+          ),
+          SizedBox(
+            height: 88,
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        width: double.infinity,
+        height: 88,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    product.title.toString(),
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
-                    ),
+                  SizedBox(
+                    height: 10,
                   ),
                   Text(
-                    product.subTitle.toString(),
+                    "Price",
                     style: TextStyle(
-                      fontSize: 15,
-                      color: Color(0xff9B9B9B),
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "⭐${product.rating}",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        " (${product.voteCount})",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Spacer(),
-                      Container(
-                        margin: EdgeInsets.only(right: 10),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Color(0xffF9F9F9),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Icon(
-                          Icons.coffee,
-                          color: Color(0xffC67C4E),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Color(0xffF9F9F9),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Icon(
-                          Icons.coffee,
-                          color: Color(0xffC67C4E),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    thickness: .5,
-                  ),
-                  Text(
-                    "Description",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  ReadMoreText(
-                    "${product.description}",
-                    trimMode: TrimMode.Line,
-                    trimLines: 1,
-                    colorClickableText: Color(0xffC67C4E),
-                    trimCollapsedText: 'Read more',
-                    trimExpandedText: 'Show less',
-                    moreStyle: TextStyle(
-                      color: Color(0xffC67C4E),
                       fontSize: 14,
-                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
                     ),
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
                   Text(
-                    "Size",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                  Row(
-                    children: [
-                      CoffeeSizesButtonwidget(
-                        size: CoffeeSizes.S
-                            .toString()
-                            .split("CoffeeSizes.")
-                            .join(),
-                      ),
-                      CoffeeSizesButtonwidget(
-                        size: CoffeeSizes.M
-                            .toString()
-                            .split("CoffeeSizes.")
-                            .join(),
-                      ),
-                      CoffeeSizesButtonwidget(
-                        size: CoffeeSizes.L
-                            .toString()
-                            .split("CoffeeSizes.")
-                            .join(),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 96,
+                    "\$ ${product.price}",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xffC67C4E),
+                        fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
-            ),
-            Text("Price"),
-            Container(
-              width: 400,
-              height: 80,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  )),
-            ),
-          ],
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(40))
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    
+                    backgroundColor: MaterialStatePropertyAll(
+                      Color(0xffC67C4E),
+                    ),
+                    padding: MaterialStatePropertyAll(
+                      EdgeInsets.symmetric(
+                        horizontal: 80,
+                        vertical: 20,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
